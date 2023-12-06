8ae075effa4ca64dfa131e8c978ba084d36918fd
sky <adsky0309@gmail.com>
2023-12-06 17:18:14 +0900

    dfsdfs

---
+++ docs/polls_first_sky.py
@@ -0,0 +1,26 @@
+list_question = [
+    "상품의 품질에 대해 어떻게 생각하시나요?",
+    "상품의 가격에 대해 어떻게 생각하시나요?",
+    "상품의 디자인에 대해 어떻게 생각하시나요?",
+    "상품에 대한 전반적인 만족도는 어떠신가요?"
+]
+list_answer = ["좋음", "중간", "좋아지길"]
+
+for num_question_count in range(len(list_question)):
+    print("{}. {}".format(num_question_count+1, list_question[num_question_count]))
+    for num_answer_count in range(len(list_answer)):
+        print("{}. {}".format(num_answer_count+1, list_answer[num_answer_count]), end=" ")
+    if num_question_count!=3 : 
+            print("")
+            print("----------")
+    elif num_question_count==3 :
+        break
+    
+
+
+
+
+
+# for int str
+#
+# "1. {} 2. {} 3. {}".format(list_answer[0],list_answer[1], list_answer[2])
