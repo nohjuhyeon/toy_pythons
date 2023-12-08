0df4813a6bb175a78cb581a9bf31079f8d77dcef
nohjuhyeon <njh2720@gmail.com>
2023-12-08 12:35:40 +0900

    fdfad

--- docs/playground/dontjudegme_second.py
+++ docs/playground/dontjudegme_second.py
@@ -8,21 +8,21 @@
 
 def result_cal() :
     score=[]
-    if user_answer[0] == correct_answer[0] :
-        score.append(int(score_list[]))
-    elif user_answer[0] != correct_answer[0] : 
-        None
-    user_sum = sum(score)
+    for i in range(len(user_answer)):
+       if user_answer[i] == correct_answer[i] :
+            score.append(int(score_list[i]))
+            user_sum = sum(score)
     if user_sum >=30 :
         user_score = "A"
     elif user_sum >=20 :
         user_score = "B"
     else:
         user_score = "C"
-    print = "--------결과---------"
-    print = "응답한 내용 : 1번 {}, 2번 {}, 3번 {}".format(user_answer(0:))
-    print = "당신 응답 합계 : {}점".format(user_sum)
-    print = "학점은 {}입니다.".format(user_score)
+    print ("--------결과---------")
+    print("응답한 내용 : 1번 {}, 2번 {}, 3번 {}, 4번 {}".format(user_answer[0], user_answer[1],user_answer[2], user_answer[3]))
+    print("당신 응답 합계 : {}점".format(user_sum))
+    print("학점은 {}입니다.".format(user_score))
+ 
 
-print(result_cal())
+result_cal()
 
