6d398ebef026303a7ebfc3f716767a2967230874
sky <adsky0309@gmail.com>
2023-12-08 11:53:32 +0900

    ㅇ

--- docs/playground/dontjudegme_second.py
+++ docs/playground/dontjudegme_second.py
@@ -1,10 +1,15 @@
-# 답을 받아서 산출하기
+# 답을 받아서 산출하기...sky
 
 
 # 입력 리스트 [] 정답리스트 [] 점수 리스트[]
 # if문으로 비교해서 점수 넣기(function쓰기 가능)
 # 최종 점수 계산식
 
+def print_method():
+    print("--------결과---------")
+    print("응답한 내용 : 1번 {}, 2번 {}, 3번 {}".format(user_answer(0:)))
+    print("당신 응답 합계 : {}점".format(user_sum))
+    print("학점은 {}입니다.".format(user_score))
 
 def result_cal() :
     score=[]
@@ -12,5 +17,14 @@ def result_cal() :
         score.append(int(score_list[]))
     elif user_answer[0] != correct_answer[0] : 
         None
+    user_sum = sum(score)
+    if user_sum >=30 :
+        user_score = "A"
+    elif user_sum >=20 :
+        user_score = "B"
+    else:
+        user_score = "C"
+    result =
 
+return result
 
