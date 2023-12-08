1572bf0fb5356d1285f158ba1e9b63d3d4702cef
sky <adsky0309@gmail.com>
2023-12-08 11:43:23 +0900

    first

---
+++ docs/playground/dontjudegme_second.py
@@ -0,0 +1,18 @@
+# 답을 받아서 산출하기
+
+
+# 입력 리스트 [] 정답리스트 [] 점수 리스트[]
+# if문으로 비교해서 점수 넣기(function쓰기 가능)
+# 최종 점수 계산식
+
+
+user_anwer
+
+score=[]
+if user_answer[0] == correct_answer[0] :
+    score.append(int(score_list[]))
+elif user_answer[0] != correct_answer[0] : 
+    None
+        
+
+
